bbb0819d6851293ad3d43486d3811417b198981a
Azimov Makhmudjon <Makhmudjon.Azimov@swisscom.com>
2024-05-13 04:41:43 +0300

    Ui changes of diffs

--- src/app/services/data-report.service.ts
+++ src/app/services/data-report.service.ts
@@ -7,12 +7,13 @@ import { DiffsModel } from '../model/diffs.model';
   providedIn: 'root',
 })
 export class DataService {
+  baseUrl = 'http://127.0.0.1:5000';
   constructor(private http: HttpClient) {}
 
   getReportData(): Observable<DiffsModel[]> {
-    return this.http.get<DiffsModel[]>('http://127.0.0.1:5000/report-data');
+    return this.http.get<DiffsModel[]>(this.baseUrl + '/report-data');
   }
   postConfigData(data: any): Observable<any> {
-    return this.http.post<any>('http://127.0.0.1:5000/config-data', data);
+    return this.http.post<any>(this.baseUrl + '/config-data', data);
   }
 }
